5b0090e6f6dae48b065c4ad2daaf9d6adab2c5e2
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2023-10-13 14:44:52 +0800

    fix spacings

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-const hamburger = document.querySelector(".hamburger");
-const btnClose = document.querySelector(".btn-close");
-const sideMenu = document.querySelector(".mobile-sidemenu");
+const hamburger = document.querySelector('.hamburger')
+const btnClose = document.querySelector('.btn-close')
+const sideMenu = document.querySelector('.mobile-sidemenu')
 
-hamburger.addEventListener("click", () => {
-  sideMenu.classList.toggle("show");
-});
-btnClose.addEventListener("click", () => {
-  sideMenu.classList.toggle("show");
-});
+hamburger.addEventListener('click', () => {
+	sideMenu.classList.toggle('show')
+})
+btnClose.addEventListener('click', () => {
+	sideMenu.classList.toggle('show')
+})
